196669eb1bbe707fd950818ca455e6d461996462
Thomas Laumonier <tlaumonier@blockchain.com>
2026-07-08 17:34:16 +0100

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://lightningmapgame.com',
+  generateRobotsTxt: true,
+  exclude: ['/at/blitzortung', '/gr/blitzortung', /* toutes les anciennes URLs */],
+}
